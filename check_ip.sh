dcd72081c2b2bc5389bd83cc8e6710477d1017b2
DingdingLuan <39515102+DingdingLuan@users.noreply.github.com>
2021-04-21 21:02:40 +0800

    Update check_ip.sh

--- check_ip.sh
+++ check_ip.sh
@@ -12,15 +12,16 @@
 ip_now=$(/usr/sbin/ifconfig |grep 'inet 202.192.33' |awk '{print $2}')
 time_now=$(echo $(date +%F-%T))
 	#判断服务器是否为断网或不在教育网202网段状态状态
+	#此网段为作者所在网段，您可自己修改为您所在网段
 	if [ ! $ip_now ];then
 		exit 0
 	fi
 	#---->断网了GG思密达
 
 ###初次运行程序创建日志文件
-if [ ! -f /home/luantch/check_ip/ip_history.txt ]; then
+if [ ! -f /home/luantch/check_ip/ip_history.txt ]; then  #此处地址应当为您自己文件所在地址，请进行修改
 	echo $time_now $ip_now > /home/luantch/check_ip/ip_history.txt
-	rm -rf /mnt/firstlight_ipcheck/ip_history.txt
+	rm -rf /mnt/firstlight_ipcheck/ip_history.txt   #此处为作者dav云盘挂载地址，请进行修改
 	cp -f /home/luantch/check_ip/ip_history.txt /mnt/firstlight_ipcheck/ip_history.txt
 	exit 0
 fi
